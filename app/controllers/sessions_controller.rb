7ea3120b6c05ceef16b837e9f98e57c738d88365
zglicz <michal.zgliczynski@uj.edu.pl>
2013-08-24 16:30:04 +0200

    More flashes and authorization

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,16 +5,18 @@ class SessionsController < ApplicationController
 	def create
 		user = User.find_by(email: params[:session][:email].downcase)
 	    if user && user.authenticate(params[:session][:password])
+	      flash[:success] = 'Succesfully logged in'
 	      sign_in user
 	      redirect_to user
 	    else
-	      flash.now[:error] = 'Invalid email/password combination' # Not quite right!
+	      flash.now[:error] = 'Invalid email/password combination'
 	      render 'new'
 	    end
 	end
 
 	def destroy
 		sign_out
+		flash[:success] = 'Succesfully logged out'
 		redirect_to root_url
 	end
 end
